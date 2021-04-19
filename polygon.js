0bdbcd44a75b9089b0b6f6ce930fe58346e0551c
Abhiremya <77828976+Abhiremya@users.noreply.github.com>
2021-04-20 02:34:38 +0530

    Add files via upload

---
+++ polygon.js
@@ -0,0 +1,31 @@
+// class Polygon
+// {
+// 	constructor(x,y,r)
+// 	{
+// 		var options={
+// 			isStatic:false,
+// 			restitution:0,
+// 			friction:1,
+// 			density:1.2
+// 			}
+		
+// 		this.image=loadImage("polygon.png");
+// 		this.body=Bodies.circle(50,200,20, options)
+// 		World.add(world, this.body);
+
+// 	}
+// 	display()
+// 	{
+		
+// 		var polygonpos=this.body.position;
+// 			push()
+// 			translate(polygonpos.x, polygonpos.y);
+// 			rectMode(CENTER)
+// 			rotate(this.body.angle)
+// 			// fill(255,0,255)
+// 			// imageMode(CENTER);
+// 			// ellipseMode(RADIUS)
+// 			image(this.image, 10,10)
+// 			pop()
+//     }
+// }
